9c3927efab00d6428925b6531cb2060c2f5343db
jmoeys <jmoeys@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-01 10:13:24 +0000

    Fixed an encoding issue with the Polish triangle under Mac

--- pkg/soiltexture/R/onAttach.R
+++ pkg/soiltexture/R/onAttach.R
@@ -2,20 +2,29 @@
  libname, 
  pkgname  
 ){  #
+    cat( "'", pkgname, "' loaded.\n" ) 
+    #
     # Setup the polish triangle and language setting
-    source( system.file( "polish_triangle.r", package = pkgname ), encoding = "UTF-8" ) 
-    source( system.file( "polish_language.r", package = pkgname ), encoding = "UTF-8" ) 
+    if( Sys.info()["sysname"] != "Mac" )
+    {   #
+        source( system.file( "polish_triangle.r", package = pkgname ), encoding = "UTF-8" ) 
+        source( system.file( "polish_language.r", package = pkgname ), encoding = "UTF-8" ) 
+        #
+        lang.par <- TT.get( "lang.par" ) 
+        lang.par <- rbind( 
+            lang.par, 
+            lang.par2 
+        )   #
+        #
+        # rm( lang.par2 ) 
+        #
+        TT.set( "lang.par" = lang.par ) 
+    }else{ 
+        cat( "\tPolish diagram and language are not default loaded for Macs,\n" ) 
+        cat( "\ttype source( system.file( 'polish_triangle.r', package = pkgname ), encoding = 'UTF-8' )\n" ) 
+        cat( "\tand source( system.file( 'polish_language.r', package = pkgname ), encoding = 'UTF-8' )\n" ) 
+        cat( "\tto load them (if it works)" ) 
+    }   #
     #
-    lang.par <- TT.get( "lang.par" ) 
-    lang.par <- rbind( 
-        lang.par, 
-        lang.par2 
-    )   #
-    #
-    # rm( lang.par2 ) 
-    #
-    TT.set( "lang.par" = lang.par ) 
-    #
-    cat( "'", pkgname, "' has been loaded.\n" ) 
 }   #
 
