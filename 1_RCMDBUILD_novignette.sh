ccf03642d1a3cc30e0366d3d36523d49b4102a5f
jmoeys <jmoeys@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2011-11-21 20:46:34 +0000

    Try to get the vignette compiled on Linux

--- 1_RCMDBUILD_novignette.sh
+++ 1_RCMDBUILD_novignette.sh
@@ -1,4 +1,4 @@
-cd "/home/jules/_WORK/R_PACKAGES/soiltexture/pkg"
+cd "/home/jules/Documents/_WORK/R_PACKAGES/soiltexture/pkg"
 R CMD build --no-vignettes soiltexture 
 echo "press any key to continue"; read line
 
