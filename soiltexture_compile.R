83618a55e822903eef5186f4a29dade8daa5b1b0
jmoeys <jmoeys@edb9625f-4e0d-4859-8d74-9fd3b1da38cb>
2012-01-25 16:49:36 +0000

    Attempt to fix package dependence in the vignette (xtable)

--- soiltexture_compile.R
+++ soiltexture_compile.R
@@ -2,9 +2,9 @@ rm(list=ls(all=TRUE))
 pkg.dir      <- "C:/_R_PACKAGES/soiltexture/pkg" 
 # pkg.dir      <- "/home/jules/_WORK/R_PACKAGES/soiltexture/pkg" 
 pkg.name     <- "soiltexture" 
-pkg.version  <- "1.2.7" 
+pkg.version  <- "1.2.8" 
 pkg.depends  <- c("sp","MASS") 
-pkg.suggests <- c("drc","plotrix") 
+pkg.suggests <- c("drc","plotrix","xtable") 
 RVersion     <- "R (>= 2.4.1)" 
 # r.path       <- "C:/Program Files/_SCIENCE/R_PROJECT_2-4-1/bin" 
 r.path       <- ""  #  Use curent R version
